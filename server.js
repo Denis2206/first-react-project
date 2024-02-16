9700316e3a63be756d70557aa8c5429f973433b4
Денис <destinysmile@yandex.ru>
2024-02-16 19:53:51 +0300

    edit server and component

--- server.js
+++ server.js
@@ -1,26 +1,65 @@
 const express = require("express");
 const cors = require("cors");
+const {MongoClient, ObjectId} = require("mongodb");
 
 const app = express();
+const mongo_uri = "mongodb://127.0.0.1:27017";
+const db_name = "todoListDb";
+const collection_name = "todos";
+// let db; // база данных
+let db; // база данных
+
 app.use(express.json());
 app.use(cors());
 
-let todos = [];
+// let todos = [];
+MongoClient.connect(mongo_uri)
+    .then((client) => {
+        console.log("Connected to MongoDB");
+        db = client.db(db_name);
+    })
+    .catch((error) => console.error("Error connecting to MongoDB", error));
 
-app.get("/api/todos", (req, res) => {
-    res.json(todos);
+// получение списка задач
+app.get("/api/todos", async (req, res) => {
+    // res.json(todos);
+    try {
+        const todos = await db.collection(collection_name).find().toArray();
+        res.json(todos);
+    } catch (error) {
+        console.error("Error fetching todos: ", error);
+        res.status(500).json({message: "Error fetching todos"});
+    }
 });
 
-app.post("/api/todos", (req, res) => {
-    const newTodo = req.body.todo;
-    todos.push(newTodo);
-    res.json({message: "ToDo added succesfully"});
+// добавление новых задач
+app.post("/api/todos", async (req, res) => {
+    // todos.push(newTodo);
+    try {
+        // const newTodo = req.body.todo;
+        const {todo, priority, deadline} = req.body;
+        // await db.collection(collection_name).insertOne({todo: newTodo});
+        await db.collection(collection_name).insertOne({todo, priority, deadline});
+        res.json({message: "ToDo added succesfully"});
+    } catch (error) {
+        console.error("Error adding todos: ", error);
+        res.status(500).json({message: "Error adding todos"});
+    }
 });
 
-app.delete("/api/todos/:index", (req, res) => {
-    const index = req.params.index;
-    todos.splice(index, 1);
-    res.json({message: "ToDos deleted succesfully"});
+// удаление
+// app.delete("/api/todos/:index", (req, res) => {
+app.delete("/api/todos/:id", async (req, res) => {
+    // const index = req.params.index;
+            // todos.splice(index, 1);
+    try {
+        const id = new ObjectId(req.params.id);
+        await db.collection(collection_name).deleteOne({_id: id});
+        res.json({message: "ToDos deleted succesfully"});
+    } catch (error) {
+        console.error("Error deleting todos: ", error);
+        res.status(500).json({message: "Error deleting todos"});
+    }
 });
 
 app.listen(8888, () => {
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -3,8 +3,9 @@ import axios from "axios";
 
 const ToDoList = () => {
     const [todos, setTodos] = useState([]);
-    const [inputValue, setInputValue] = useState("");
-
+    const [inputValue, setInputValue] = useState(""); // значение в поле ввода задачи
+    const [priority, setPriority] = useState("Low"); // приоритет
+    const [deadline, setDeadline] = useState(""); // дедлайн
     // загрузка задач из locarstorage при запуске приложения
     // useEffect(() => {
     //     const savedTodos = localStorage.getItem("todos");
@@ -30,9 +31,13 @@ const ToDoList = () => {
     //     localStorage.setItem("todos", JSON.stringify(todos));
     // }, [todos]);
 
-    const handlerInputChange = (event) => setInputValue(event.target.value);
+    const handleInputChange = (event) => setInputValue(event.target.value);
 
-    const handleAddToDo = async() => {
+    const handlePriorityChange = (event) => setPriority(event.target.value);
+
+    const handleDeadlineChange = (event) => setDeadline(event.target.value);
+
+    const handleAddToDo = async () => {
         // // обрезаем пустые символы
         // if (inputValue.trim() !== "") {
         //     // добавляем новое значение в конец существующего массива
@@ -40,21 +45,29 @@ const ToDoList = () => {
         //     setInputValue("");
         // }
         try {
-            await axios.post("http://localhost:8888/api/todos", {todo: inputValue});
+            await axios.post("http://localhost:8888/api/todos", {
+                todo: inputValue,
+                priority: priority,
+                deadline: deadline,
+            });
             fetchToDos();
             setInputValue("");
+            setPriority("Low");
+            setDeadline("");
         } catch (error) {
             console.error("Error adding todo: ", error);
         }
     };
 
-    const handleDeleteToDo = async(index) => {
+    // const handleDeleteToDo = async(index) => {
+    const handleDeleteToDo = async (id) => {
         // // сохранение в новый массив элементов, не соответствующих номеру переданного на удаление
         // const newTodos = todos.filter((todo, i) => i !== index);
         // setTodos(newTodos);
 
         try {
-            await axios.delete(`http://localhost:8888/api/todos/${index}`);
+            // await axios.delete(`http://localhost:8888/api/todos/${index}`);
+            await axios.delete(`http://localhost:8888/api/todos/${id}`);
             fetchToDos();
         } catch (error) {
             console.error("Error deleting todo: ", error);
@@ -64,16 +77,35 @@ const ToDoList = () => {
     return (
         <div className="container">
             <h1 className="text-center mb-4">ToDo List</h1>
-            <input type="text" className="form-control my-3" value={inputValue} onChange={handlerInputChange} placeholder="Enter new todo..." />
+            <div className="form-group my-2">
+                <input type="text" className="form-control my-3" value={inputValue} onChange={handleInputChange} placeholder="Enter new todo..." />
+                <select value={priority} onChange={handlePriorityChange} className="form-select my-2">
+                    <option value="" disabled>Choose priority</option>
+                    <option value="Low">Low</option>
+                    <option value="Medium">Medium</option>
+                    <option value="High">High</option>
+                </select>
+                <input type="datetime-local" value={deadline} onChange={handleDeadlineChange} className="form-control my-2" />
+            </div>
             <button className="btn btn-outline-primary my-3" onClick={handleAddToDo}>
                 Save
             </button>
-
             <ul className="list-group mt-3">
-                {todos.map((todo, index) => (
+                {/* {todos.map((todo, index) => (
                     <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
                         {todo}
                         <button className="btn btn-outline-danger" onClick={(() => handleDeleteToDo(index))}>
+                            Delete
+                        </button>
+                    </li>
+                ))} */}
+                {todos.map((todo) => (
+                    <li key={todo._id} className="list-group-item d-flex justify-content-between align-items-center pb-2">
+                        <span>Name: {todo.todo}</span>|
+                        <span>Priority: {todo.priority}</span>|
+                        <span>Deadline: {todo.deadline}</span>
+
+                        <button className="btn btn-outline-danger" onClick={() => handleDeleteToDo(todo._id)}>
                             Delete
                         </button>
                     </li>
